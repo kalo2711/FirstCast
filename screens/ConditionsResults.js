15799f132942f099eaab8620789172eb14aa5e57
Stefano Scalzo <stefanoscalzo@Stefanos-MacBook-Air.local>
2024-04-12 15:35:54 -0400

    feat/test-expo fix nav

--- screens/ConditionsResults.js
+++ screens/ConditionsResults.js
@@ -1,13 +1,14 @@
-import React from "react";
+import {React , useState} from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function ConditionsResults({ route }) {
-  const { name, age } = route.params;
+  const [name, setName] = useState(route?.params?.name);
+  const [age, setAge] = useState(route?.params?.age);
 
   return (
     <View style={styles.container}>
       <Text>
-        You are {name} and you are {age} years old.
+        You are {name ? name :'def'} and you are {age ? age : 'age'} years old.
       </Text>
     </View>
   );
