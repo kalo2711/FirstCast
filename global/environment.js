8a55b518c6f8e9decfce87edaf2d7aefa3da9dc9
Stefano Scalzo <stefanoscalzo@Stefanos-MacBook-Air.local>
2024-06-30 17:35:44 -0400

    put env back

--- global/environment.js
+++ global/environment.js
@@ -1,14 +1,14 @@
-export const environment = {
-  production: false,
-  host: "http://192.168.0.21:3000/",
-  authHost: "http://192.168.0.21:3001/",
-  messagingHost: "http://192.168.0.21:3002/",
-  imageBucket: "https://storage.googleapis.com/puggum-bucket/",
-};
-
 // export const environment = {
-//   production: true,
-//   host: "https://firstcast.org/main/",
-//   authHost: "https://firstcast.org/auth/",
-//   imageBucket: "https://storage.googleapis.com/puggum-bucket/"
-// }
+//   production: false,
+//   host: "http://192.168.0.21:3000/",
+//   authHost: "http://192.168.0.21:3001/",
+//   messagingHost: "http://192.168.0.21:3002/",
+//   imageBucket: "https://storage.googleapis.com/puggum-bucket/",
+// };
+
+export const environment = {
+  production: true,
+  host: "https://firstcast.org/main/",
+  authHost: "https://firstcast.org/auth/",
+  imageBucket: "https://storage.googleapis.com/puggum-bucket/"
+}
